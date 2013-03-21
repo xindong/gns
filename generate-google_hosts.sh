958023ba3820a6ad52accbd2b65839fbdcfb76a1
xdanger <xdanger@gmail.com>
2013-03-22 00:57:35 +0800

    first commit

---
+++ generate-google_hosts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ip_prefix=$1
+tmp="/tmp/_.https.ip"
+
+i=0
+while [[ $i -lt 256 ]]; do
+    ip="$ip_prefix.$i"
+    echo "Detecting $ip ... " >&2
+    curl -m 1 https://$ip/ > /dev/null 2>$tmp
+    err=`grep 'does not match target host name' $tmp`
+    if [[ `echo $err | grep -v ^$ | wc -l` -gt 0 ]]; then
+        dmn=`echo $err | sed "s#^[^']\+'\([^']\+\)'.\+#\1#g"`
+        echo $ip $dmn >> google_ip-$ip_prefix
+        echo
+    fi
+    let i=i+1
+done
---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+echo gns.*.conf > dnsmasq.gns.conf
