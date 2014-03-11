7616956d1dd941af78a070bc3d89997ffddc5930
xdanger <xdanger@gmail.com>
2014-03-11 22:38:48 +0800

    扫描整个Google中国网段：203.208.32.0 - 203.208.63.255

--- bin/generate-google_hosts.sh
+++ bin/generate-google_hosts.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-ip_prefix=$1
+ip_prefix="203.208"
 tmp="/tmp/_.https.ip"
 
 cd `dirname $0`
 
-i=0
-while [[ $i -lt 256 ]]; do
-    ip="$ip_prefix.$i"
-    echo "Detecting $ip ... " >&2
-    curl -m 1 https://$ip/ > /dev/null 2>$tmp
-    err=`grep 'does not match target host name' $tmp`
-    if [[ `echo $err | grep -v ^$ | wc -l` -gt 0 ]]; then
-        dmn=`echo $err | sed "s#^[^']\+'\([^']\+\)'.\+#\1#g"`
-        echo $ip $dmn >> ../build/google_ip-$ip_prefix
-        echo
-    fi
-    let i=i+1
+x=32
+while [[ $x -lt 64 ]]; do
+    y=0
+    while [[ $y -lt 256 ]]; do
+        ip="$ip_prefix.$x.$y"
+        echo "Detecting $ip ... " >&2
+        curl -m 1 https://$ip/ > /dev/null 2>$tmp
+        err=`grep 'does not match target host name' $tmp`
+        if [[ `echo $err | grep -v ^$ | wc -l` -gt 0 ]]; then
+            dmn=`echo $err | sed "s#^[^']\+'\([^']\+\)'.\+#\1#g"`
+            echo $ip $dmn >> ../build/google_ip-$ip_prefix
+            echo
+        fi
+        let y=y+1
+    done
+    let x=x+1
 done
+
