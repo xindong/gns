2a321d6062c4ad12b2f82790bef57ddae31214ef
吴涵 <wuhan0608@gmail.com>
2016-02-03 17:56:22 +0800

    Update make.sh: enable `log-facility`

--- make.sh
+++ make.sh
@@ -10,6 +10,7 @@ echo "resolv-file=/etc/dnsmasq.resolv" >> $build
 echo "addn-hosts=/etc/dnsmasq.hosts" >> $build
 echo "conf-dir=/etc/dnsmasq.d" >> $build
 echo "log-queries" >> $build
+echo "log-facility=local1" >> $build
 #echo "cache-size=20000" >> $build
 
 echo >> $build
