f325e62e4584e894f9e4b835b02fe6e03719b258
Tomasen <tomasen@gmail.com>
2016-02-15 16:57:38 +0800

    国内访问itunesconnect实在太慢了；加入min-cache-ttl，cache 至少5分钟内不过期

--- make.sh
+++ make.sh
@@ -6,14 +6,14 @@ build="build/dnsmasq.conf"
 
 echo -n > $build
 
+echo "min-cache-ttl=600" >> $build
 echo "resolv-file=/etc/dnsmasq.resolv" >> $build
 echo "addn-hosts=/etc/dnsmasq.hosts" >> $build
 echo "conf-dir=/etc/dnsmasq.d" >> $build
 echo "log-queries" >> $build
 echo "log-facility=local1" >> $build
-#echo "cache-size=20000" >> $build
+echo "cache-size=20000" >> $build
 
 echo >> $build
 
 awk 1 conf/gns.*.conf >> $build
-
